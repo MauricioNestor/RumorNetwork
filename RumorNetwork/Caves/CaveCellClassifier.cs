9b4fa82babf64c86b14c76d24a058fcbf748167f
MauricioNestor <mauricio7410@gmail.com>
2026-07-22 11:01:05 -0300

    Split cave cell classification responsibilities

--- RumorNetwork/Caves/CaveCellClassifier.cs
+++ RumorNetwork/Caves/CaveCellClassifier.cs
@@ -1,21 +1,22 @@
-using System;
 using Vintagestory.API.Common;
 using Vintagestory.API.MathTools;
-using Vintagestory.GameContent;
 
 namespace RumorNetwork.Caves
 {
     public sealed class CaveCellClassifier
     {
-        private const float FullBlockTolerance = 0.0001f;
-
         private readonly IBlockAccessor blockAccessor;
+        private readonly CaveObstacleClassifier obstacleClassifier;
 
         public CaveCellClassifier(
             IBlockAccessor blockAccessor
         )
         {
             this.blockAccessor = blockAccessor;
+            obstacleClassifier =
+                new CaveObstacleClassifier(
+                    blockAccessor
+                );
         }
 
         public CaveCellInfo Classify(
@@ -29,7 +30,9 @@ namespace RumorNetwork.Caves
                 );
 
             CaveMedium medium =
-                ClassifyMedium(fluidBlock);
+                CaveMediumClassifier.Classify(
+                    fluidBlock
+                );
 
             if (medium == CaveMedium.Lava)
             {
@@ -54,7 +57,7 @@ namespace RumorNetwork.Caves
                 );
 
             CaveTraversalKind traversal =
-                ClassifyObstacle(
+                obstacleClassifier.Classify(
                     obstacleBlock,
                     position
                 );
@@ -63,122 +66,6 @@ namespace RumorNetwork.Caves
                 traversal,
                 medium
             );
-        }
-
-        private CaveTraversalKind ClassifyObstacle(
-            Block block,
-            BlockPos position
-        )
-        {
-            BlockBehaviorDoor? doorBehavior =
-                block.GetBehavior<BlockBehaviorDoor>();
-
-            if (doorBehavior != null)
-            {
-                if (!doorBehavior.handopenable)
-                {
-                    return CaveTraversalKind.Blocked;
-                }
-
-                if (IsBarLockedDoor(position))
-                {
-                    return CaveTraversalKind.Blocked;
-                }
-
-                return CaveTraversalKind.Openable;
-            }
-
-            Cuboidf[]? collisionBoxes =
-                block.GetCollisionBoxes(
-                    blockAccessor,
-                    position
-                );
-
-            if (
-                collisionBoxes == null ||
-                collisionBoxes.Length == 0
-            )
-            {
-                return CaveTraversalKind.Open;
-            }
-
-            return HasFullBlockCollision(collisionBoxes)
-                ? CaveTraversalKind.Blocked
-                : CaveTraversalKind.Unknown;
-        }
-
-        private bool IsBarLockedDoor(
-            BlockPos position
-        )
-        {
-            BlockEntity? blockEntity =
-                blockAccessor.GetBlockEntity(position);
-
-            return blockEntity?
-                .GetBehavior<BEBehaviorDoorBarLock>()
-                != null;
-        }
-
-        private static CaveMedium ClassifyMedium(
-            Block fluidBlock
-        )
-        {
-            if (
-                fluidBlock.Id == 0 ||
-                !fluidBlock.IsLiquid()
-            )
-            {
-                return CaveMedium.Dry;
-            }
-
-            if (
-                fluidBlock is IBlockFlowing flowingBlock &&
-                flowingBlock.IsLava
-            )
-            {
-                return CaveMedium.Lava;
-            }
-
-            if (
-                string.Equals(
-                    fluidBlock.LiquidCode,
-                    "water",
-                    StringComparison.OrdinalIgnoreCase
-                )
-            )
-            {
-                return CaveMedium.Water;
-            }
-
-            return CaveMedium.OtherFluid;
-        }
-
-        private static bool HasFullBlockCollision(
-            Cuboidf[] collisionBoxes
-        )
-        {
-            foreach (Cuboidf box in collisionBoxes)
-            {
-                if (IsFullBlock(box))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        private static bool IsFullBlock(
-            Cuboidf box
-        )
-        {
-            return
-                box.X1 <= FullBlockTolerance &&
-                box.Y1 <= FullBlockTolerance &&
-                box.Z1 <= FullBlockTolerance &&
-                box.X2 >= 1 - FullBlockTolerance &&
-                box.Y2 >= 1 - FullBlockTolerance &&
-                box.Z2 >= 1 - FullBlockTolerance;
         }
     }
 }
