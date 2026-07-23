ff4f836aa2b4dca2b05bcac07208e01d616eeb76
MauricioNestor <mauricio7410@gmail.com>
2026-07-22 22:45:58 -0300

    Detect first verified discovery migration

--- RumorNetwork/Catalog/VerifiedStructureDiscoverySaveData.cs
+++ RumorNetwork/Catalog/VerifiedStructureDiscoverySaveData.cs
@@ -7,7 +7,7 @@ namespace RumorNetwork.Catalog
     public sealed class VerifiedStructureDiscoverySaveData
     {
         [ProtoMember(1)]
-        public int Version { get; set; } = 1;
+        public int Version { get; set; }
 
         [ProtoMember(2)]
         public List<long> InspectedChunkIndices
